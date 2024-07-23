ad61b829663c0e462cf4be98bb3525c810f07a7d
joberle42 <93990401+joberle42@users.noreply.github.com>
2024-07-23 13:49:36 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(useGSAP);
+}
+
+export default function Home() {
+  const container = useRef<HTMLElement | any>();
+  const tl = useRef<GSAPTimeline | any>();
+
+  const toggleTimeline = () => {
+    tl.current.reversed(!tl.current.reversed());
+  };
+
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray('.box');
+      tl.current = gsap
+        .timeline()
+        .to(boxes[0], { x: 120, rotation: 360 })
+        .to(boxes[1], { x: -120, rotation: -360 }, '<')
+        .to(boxes[2], { y: -166 })
+        .reverse();
+    },
+    { scope: container }
+  );
+
+  return (
+    <main>
+      <section className="boxes-container" ref={container}>
+        <h2>Use the button to toggle a Timeline</h2>
+        <div>
+          <button onClick={toggleTimeline}>Toggle Timeline</button>
+        </div>
+        <div className="box gradient-blue">Box 1</div>
+        <div className="box gradient-blue">Box 2</div>
+        <div className="box gradient-blue">Box 3</div>
+      </section>
+    </main>
+  );
+}
